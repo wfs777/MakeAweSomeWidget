02732c4f4ea14940cc7d0e0ca139e26583ebfb6e
zhuruiqi6 <zhuruiqi6@hikvision.com.cn>
2025-04-02 14:26:06 +0800

    Initial Commit

---
+++ MakeAwesomeWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MakeAwesomeWidget
+//
+//  Created by zhuruiqi6 on 2025/4/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MakeAwesomeWidget/MakeAwesomeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeAwesomeWidgetApp.swift
+//  MakeAwesomeWidget
+//
+//  Created by zhuruiqi6 on 2025/4/2.
+//
+
+import SwiftUI
+
+@main
+struct MakeAwesomeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
